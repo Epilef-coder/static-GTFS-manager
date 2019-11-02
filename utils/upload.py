92d71909a9ace54a8c83b8905cc91fe9bddb77a5
Martijn van Laar <martijn@mvanlaar.net>
2019-11-02 21:02:22 +0100

    bugfix 006
    upload bug with edge on windows

--- utils/upload.py
+++ utils/upload.py
@@ -7,7 +7,11 @@ def uploadaFile(fileholder):
     # receiving a form file object as argument.
     # saving to uploadFolder. In case same name file already exists, over-writing.
 
-    filename = fileholder['filename'].replace("/", "")
+    # https://www.geeksforgeeks.org/python-os-path-split-method/
+    # Fix upload bug to allow zip file upload with MS Edge on windows.
+    # tail file contain the filename and head the path.
+    head, tail = os.path.split(fileholder['filename'])
+    filename = tail
     # zapping folder redirections if any
 
     logmessage('Saving filename: ' + filename + ' to ' + uploadFolder)
@@ -15,7 +19,7 @@ def uploadaFile(fileholder):
     if not os.path.exists(uploadFolder):
         os.makedirs(uploadFolder)
 
-    with open(uploadFolder+filename, "wb") as out:
+    with open(os.path.join(uploadFolder, filename), "wb") as out:
         # Be aware, that the user may have uploaded something evil like an executable script ...
         # so it is a good idea to check the file content (xfile['body']) before saving the file
         out.write(fileholder['body'])
